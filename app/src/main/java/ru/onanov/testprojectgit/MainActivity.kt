a3e86dd1e123114b3cba3627764184484bdeb7e2
Aleksey Onanov <mesquake@gmail.com>
2020-10-21 21:54:18 +0300

    Добавлен диалог.

--- app/src/main/java/ru/onanov/testprojectgit/MainActivity.kt
+++ app/src/main/java/ru/onanov/testprojectgit/MainActivity.kt
@@ -2,10 +2,13 @@ package ru.onanov.testprojectgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AlertDialog.Builder(this)
     }
 }
